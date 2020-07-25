5fc88ea7190e434d6cfcbd6005e4df1d6d2dab1a
alexqwerty2000 <ivanov.alex1@gmail.com>
2020-07-25 18:55:58 +0300

    Fixed enters

--- dictionary.py
+++ dictionary.py
@@ -7,11 +7,26 @@ def translate(w):
     w = w.lower()
     if w in data: 
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys()))>0:
-        return "Do you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        yn = input("Do you mean %s instead? Enter Y if Yes, or N if No: " % get_close_matches(w, data.keys())[0])
+        if yn.lower() == 'y':
+            return  data[get_close_matches(w, data.keys())[0]]
+        elif yn.lower() == 'n':
+            return "Word doesn't exist. Please double check it."
+        else:
+            return "We don't understand your enter."
     else:
-        return "Word doesn't exist. Please double check it"
+        return "Word doesn't exist. Please double check it."
 
 word = input("Enter the word: ")
 
-print(translate(word))
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
